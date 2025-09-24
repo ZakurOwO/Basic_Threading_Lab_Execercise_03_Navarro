a4462f2368fc3b6449757a468f35b6d5862a4633
ZakurOwO <johndanielnavarro130@gmail.com>
2025-09-24 19:18:19 +0800

    Add project files.

---
+++ Basic_Threading_Lab_Execercise_03_Navarro/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Basic_Threading_Lab_Execercise_03_Navarro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartandStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-Before starting thread-";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnStartandStop
+            // 
+            this.btnStartandStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnStartandStop.Location = new System.Drawing.Point(92, 72);
+            this.btnStartandStop.Name = "btnStartandStop";
+            this.btnStartandStop.Size = new System.Drawing.Size(160, 84);
+            this.btnStartandStop.TabIndex = 1;
+            this.btnStartandStop.Text = "Start";
+            this.btnStartandStop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 217);
+            this.Controls.Add(this.btnStartandStop);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartandStop;
+    }
+}
+
---
+++ Basic_Threading_Lab_Execercise_03_Navarro/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Basic_Threading_Lab_Execercise_03_Navarro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basic_Threading_Lab_Execercise_03_Navarro")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5fce8e6f-539e-4d92-b898-06c643dd9578")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
